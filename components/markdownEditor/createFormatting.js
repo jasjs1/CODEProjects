ee89dc6979dce153c5782924a617cfb56878e627
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-06-09 08:03:06 -0500

    Update createFormatting.js

---
+++ components/markdownEditor/createFormatting.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraphs = document.getElementsByTagName("p");
+    for (var i = 0; i < paragraphs.length; i++) {
+      var paragraph = paragraphs[i];
+      var paragraphText = paragraph.innerHTML;
+      if (paragraphText.indexOf("##") !== -1) {
+        paragraph.innerHTML = '<span class="custom-text">' + paragraphText + '</span>';
+      } else if (paragraphText.indexOf("#") !== -1) {
+        paragraph.innerHTML = '<span class="bigger-text">' + paragraphText + '</span>';
+      }
+    }
+  });
+
+  function addTitle() {
+    const textarea = document.getElementById('project-content');
+    textarea.value += '# ';
+    textarea.style.fontSize = '18px';
+    textarea.style.fontWeight = 'bold';
+  }
+
+  function addSubHeader() {
+    const textarea = document.getElementById('project-content');
+    textarea.value += '## ';
+    textarea.style.fontSize = '16px';
+  }
+
+  function addBoldParagraph() {
+    const textarea = document.getElementById('project-content');
+    textarea.value += '###';
+    textarea.style.fontSize = '14px';
+  }
